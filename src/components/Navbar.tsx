74ca279c58e3e02d2a9e86d35f06a39465c68c83
janith jayarisi wijesinghe <100828158+janithwijesinghe@users.noreply.github.com>
2026-03-24 14:19:11 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,12 +36,12 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 md:h-20">
           <Link to="/" className="flex items-center">
-<img
-  src="/logo.png"
-  alt="Logo"
-  className="h-20 md:h-20 object-contain"
-/>
-</Link>
+            <img
+              src="/logo.png"
+              alt="Logo"
+              className="h-20 md:h-20 object-contain"
+            />
+          </Link>
 
           <div className="hidden md:flex items-center space-x-8">
             <button
